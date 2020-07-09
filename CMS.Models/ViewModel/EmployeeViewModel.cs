748f52c0d7b62e605bfe2374bc418198dc2e9a0c
Ali Zubair <61495779+AliZ801@users.noreply.github.com>
2020-07-09 11:41:16 +0300

    Comit 1

---
+++ CMS.Models/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMS.Models.ViewModel
+{
+    public class EmployeeViewModel
+    {
+        public IEnumerable<ConfAttendees> ConfAttendees { get; set; }
+    }
+}
---
+++ CMS/Areas/User/Controllers/ConfAttendee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMS.DataAccess.Data.IRepository;
+using CMS.Models.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Areas.User.Controllers
+{
+    [Area("User")]
+    [Authorize]
+    public class ConfAttendee : Controller
+    {
+        private readonly IUnitofWork _unitofWork;
+
+        [BindProperty]
+        public EmployeeViewModel EVM { get; set; }
+
+        public ConfAttendee(IUnitofWork unitofWork)
+        {
+            _unitofWork = unitofWork;
+        }
+
+        public IActionResult Index()
+        {
+            EVM = new EmployeeViewModel() { ConfAttendees = _unitofWork.ConfAttendee.GetAll(includeProperties: "Conference,Attendees") };
+
+            return View(EVM);
+        }
+    }
+}
